b1438473f44c5157a80f4afee8b84f00e67a09d8
sangyoonLEE-web <abcbcd1540@gmail.com>
2026-04-15 14:58:30 +0900

    javascript 수업 함수까지

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// window.addEventListener("load", function()){}
+
+// window.onload = function(){실행코드}
+
+
+// 변수를 요소에 담는다
+
+        const heading = document.querySelector("#heading")
+        heading.onclick = function(){
+        heading.style.color = "red"
+        }
+        
